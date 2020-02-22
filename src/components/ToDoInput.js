db61f9d151dd5ebdf46e7d113767d6d48ddfaa6f
Łukasz <riraseth@gmail.com>
2020-02-22 18:41:17 +0100

    Update ToDoInput.js

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -3,11 +3,32 @@ import TodoItem from "./TodoItem";
 
 export default class ToDoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
-      <section>
-        <h2>todo list</h2>
-        <TodoItem></TodoItem>
-      </section>
+      <div className='card card-body my-3'>
+        <form onSubmit={handleSubmit}>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <div className='input-group-text bg-primary text-white'>
+                <i className='fas fa-book'></i>
+              </div>
+            </div>
+            <input
+              type='text'
+              className='form-control text-capitalize'
+              placeholder='add todo item'
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type='submit'
+            className='btn btn-block btn-primary mt-3 text-uppercase'
+          >
+            add task
+          </button>
+        </form>
+      </div>
     );
   }
 }
